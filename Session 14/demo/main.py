899ff5f113ffb4ca6e01d09ab8c3e1c55d272f66
An <nguyenquangan318@gmail.com>
2026-07-13 08:14:41 +0700

    Session 14

---
+++ Session 14/demo/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import text
+from database import get_db
+from routers.classrooms import router as classrooms_router
+from models.classrooms import Base
+from database import engine
+
+app = FastAPI()
+
+Base.metadata.create_all(engine)
+
+@app.get('/test-connection')
+def test_connect(db: Session = Depends(get_db)):
+    try:
+        db.execute(text('SELECT 1'))
+        return{
+            "messsage": "Success!"
+        }
+    except Exception as err:
+        return {
+            "messsage": str(err)
+        }
+
+app.include_router(classrooms_router)
